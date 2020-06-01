d5472040d7cf7e51d7c492d93fd4d6e7d3d92ed4
Amar Catovic <amarzenica@gmail.com>
2020-06-01 17:47:13 +0200

    Vozac bug fix

--- Screens/Vozac/main.js
+++ Screens/Vozac/main.js
@@ -1,21 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import { Alert, Text, View, StyleSheet, Button, TouchableOpacity } from 'react-native';
-import { BarCodeScanner } from 'expo-barcode-scanner';
-import getIP from '../../config'
+import React, { useState, useEffect } from "react";
+import {
+  Alert,
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import getIP from "../../config";
 
-export default function main({route, navigation}) {
-  const [hasPermission, setHasPermission] = useState(null)
-  const [scanned, setScanned] = useState(false)
-  const [isZaduzen, setZaduzen] = useState(false)
-  const [registracija, setRegistracija] = useState('')
-  const [hasLinija, setLinija] = useState(false)
+export default function main({ route, navigation }) {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [isZaduzen, setZaduzen] = useState(false);
+  const [registracija, setRegistracija] = useState("");
+  const [hasLinija, setLinija] = useState(false);
+  const [voziloID, setVozilo] = useState(null);
 
-  const korisnik = route.params.data
+  const korisnik = route.params.data;
 
   useEffect(() => {
     (async () => {
       const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
+      setHasPermission(status === "granted");
     })();
   }, []);
 
@@ -23,64 +31,80 @@ export default function main({route, navigation}) {
     setScanned(true);
 
     fetch(`http://${getIP()}/publictransportcloud/api/VozacVozilo/create.php`, {
-    method: 'POST', 
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({vozac_id: korisnik.id, vozilo_id: data}),
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ vozac_id: korisnik.id, vozilo_id: data }),
     })
-    .then(response => response.json())
-    .then(dataJSON => {
-      setZaduzen(true)
-      setRegistracija(data)
-      alert(`Vozilo registracije ${data} je zaduženo! Izaberite relaciju i započnite liniju`)
-    })
-    .catch((error) => {
-        alert("Vozilo nije zadruženo. Desila se greška. Pokušajte ponovno.")
-    })
-  }
+      .then((response) => response.json())
+      .then((dataJSON) => {
+        setZaduzen(true);
+        setRegistracija(data);
+        alert(
+          `Vozilo registracije ${data} je zaduženo! Izaberite relaciju i započnite liniju`
+        );
+      })
+      .catch((error) => {
+        alert("Vozilo nije zadruženo. Desila se greška. Pokušajte ponovno.");
+      });
+  };
 
   const ucitajVozacZaduzena = async () => {
-
-    await fetch(`http://${getIP()}/publictransportcloud/api/Vozac/zaduzeno-vozilo.php?id=${korisnik.id}`)
-    .then(response => response.json())
-    .then(data => {  
-        setRegistracija(data.vozilo_id)
-        setZaduzen(true)
-        setScanned(true)
-      }) 
-  }
+    await fetch(
+      `http://${getIP()}/publictransportcloud/api/Vozac/zaduzeno-vozilo.php?id=${
+        korisnik.id
+      }`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setRegistracija(data.vozilo_id);
+        if (data.status != "NO") {
+          setZaduzen(true);
+          setScanned(true);
+          setVozilo(data.vozilo_id);
+        }
+      });
+  };
 
   const ucitajVozacLinija = async () => {
-
-    await fetch(`http://${getIP()}/publictransportcloud/api/Vozac/aktivna-linija.php?id=${korisnik.id}`)
-    .then(response => response.json())
-    .then(data => {  
-        setLinija(true)
-      }) 
-  }
+    await fetch(
+      `http://${getIP()}/publictransportcloud/api/Vozac/aktivna-linija.php?id=${
+        korisnik.id
+      }`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setLinija(true);
+      });
+  };
 
   const razduziVozilo = () => {
+    setScanned(false);
 
-    setScanned(false)
-
-    fetch(`http://${getIP()}/publictransportcloud/api/VozacVozilo/zavrsi-liniju.php`, {
-    method: 'POST', 
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({vozilo_id: registracija}),
-    })
-    .then(response => response.json())
-    .then(dataJSON => {
-      alert(`Razdužili ste vozilo ` + registracija)
-      setZaduzen(false)
-    })
-    .catch((error) => {
-        alert("Vozilo nije razadruženo. Desila se greška. Pokušajte ponovno." + error + registracija)
-    })
-
-  } 
+    fetch(
+      `http://${getIP()}/publictransportcloud/api/VozacVozilo/zavrsi-liniju.php`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ vozilo_id: registracija }),
+      }
+    )
+      .then((response) => response.json())
+      .then((dataJSON) => {
+        alert(`Razdužili ste vozilo ` + registracija);
+        setZaduzen(false);
+      })
+      .catch((error) => {
+        alert(
+          "Vozilo nije razadruženo. Desila se greška. Pokušajte ponovno." +
+            error +
+            registracija
+        );
+      });
+  };
 
   if (hasPermission === null) {
     return <Text>Requesting for camera permission</Text>;
@@ -89,106 +113,127 @@ export default function main({route, navigation}) {
     return <Text>No access to camera</Text>;
   }
 
-  if(!isZaduzen)
-  {
-    ucitajVozacZaduzena()
+  if (!isZaduzen) {
+    ucitajVozacZaduzena();
     return (
       <View
         style={{
           flex: 1,
-          flexDirection: 'column',
-          justifyContent: 'flex-end',
-        }}>
+          flexDirection: "column",
+          justifyContent: "flex-end",
+        }}
+      >
         <BarCodeScanner
           onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-          style={{flex: 1, width:'100%'}}
+          style={{ flex: 1, width: "100%" }}
         />
-  
+
         {scanned && (
-          <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />
+          <Button
+            title={"Tap to Scan Again"}
+            onPress={() => setScanned(false)}
+          />
         )}
       </View>
-    )
-  }
-  else
-  {
-    ucitajVozacLinija()
+    );
+  } else {
+    ucitajVozacLinija();
     return (
       <View style={styles.container}>
-        <Text style={[styles.txt, {textAlign: 'center'}]}>Zadužili ste vozilo registarskih oznaka {registracija}</Text>
+        <Text style={[styles.txt, { textAlign: "center" }]}>
+          Zadužili ste vozilo registarskih oznaka {registracija}
+        </Text>
         <View style={styles.btns}>
           <TouchableOpacity
-            style={styles.btnNew} 
-            onPress={() => navigation.navigate("VozacLinijaManager", {data: korisnik, vozilo_id: registracija})}>
-              {hasLinija ? <Text style={color = 'white'}>Nastavi Liniju</Text> : <Text style={color = 'white'}>Nova Linija</Text>}
+            style={styles.btnNew}
+            onPress={() =>
+              navigation.navigate("VozacLinijaManager", {
+                data: korisnik,
+                vozilo_id: voziloID,
+              })
+            }
+          >
+            {hasLinija ? (
+              <Text style={(color = "white")}>Nastavi Liniju</Text>
+            ) : (
+              <Text style={(color = "white")}>Nova Linija</Text>
+            )}
           </TouchableOpacity>
-          <TouchableOpacity    
+          <TouchableOpacity
             style={styles.btnRazduzi}
-            onPress={() => Alert.alert(
-              "Upozorenje",
-              "Da li ste sigurni da želite razdužiti vozilo registarske oznake " + registracija + "?",
-              [
+            onPress={() =>
+              Alert.alert(
+                "Upozorenje",
+                "Da li ste sigurni da želite razdužiti vozilo registarske oznake " +
+                  registracija +
+                  "?",
+                [
                   {
-                  text: "Ne želim",
+                    text: "Ne želim",
                   },
-                  { text: "Želim", onPress: () => {
-                      razduziVozilo()
-                      alert("Vozilo razduženo!")
-                  }}
-              ],
-              { cancelable: false }
-          )}>
-            <Text style={color = 'white'}>Razduži Vozilo</Text>
+                  {
+                    text: "Želim",
+                    onPress: () => {
+                      razduziVozilo();
+                      alert("Vozilo razduženo!");
+                    },
+                  },
+                ],
+                { cancelable: false }
+              )
+            }
+          >
+            <Text style={(color = "white")}>Razduži Vozilo</Text>
           </TouchableOpacity>
         </View>
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    backgroundColor: '#ecf0f1',
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "#ecf0f1",
     padding: 100,
-    backgroundColor: '#e0e0e0',
-    textAlign: 'center',
-    width: '100%'
+    backgroundColor: "#e0e0e0",
+    textAlign: "center",
+    width: "100%",
   },
   btnNew: {
-    backgroundColor: '#3366CC',
-    color: 'white',
+    backgroundColor: "#3366CC",
+    color: "white",
     padding: 20,
     borderWidth: 1,
-    borderRadius:50,
+    borderRadius: 50,
     height: 30,
-    alignItems:'center',
-    justifyContent:'center',
+    alignItems: "center",
+    justifyContent: "center",
     marginTop: 50,
-    width: '100%'
+    width: "100%",
   },
   btnRazduzi: {
-    backgroundColor: '#ff6d4d',
-    color: 'white',
+    backgroundColor: "#ff6d4d",
+    color: "white",
     padding: 20,
     borderWidth: 1,
-    borderRadius:50,
+    borderRadius: 50,
     height: 30,
-    alignItems:'center',
-    justifyContent:'center',
+    alignItems: "center",
+    justifyContent: "center",
     marginTop: 50,
-    width: '100%'
+    width: "100%",
   },
   txt: {
     fontSize: 30,
-    color: '#3366CC'
+    color: "#3366CC",
   },
   btns: {
     padding: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '80%'
-  }
-})
+    justifyContent: "center",
+    alignItems: "center",
+    width: "80%",
+  },
+});
